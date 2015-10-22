a4da29f08730f2db8fe3e89b5c6afe9cc2462a65
ghrehh <gjburns9@hotmail.com>
2015-10-22 21:32:13 +0000

    got the look ahead thing working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,12 @@ class StaticPagesController < ApplicationController
   before_action :logged_in_user
   
   def dashboard
+    @recipients = []
     @invs = Inv.all.where(user_id: current_user.id).reverse
+    
+    @invs.each {|x| @recipients << [x.recipient, x.address_1, x.address_2, x.address_3] }
+    
+    @inv = Inv.new
+    @group = Group.all.where(user_id: current_user.id).reverse #finds all groups created by current user
   end
 end
