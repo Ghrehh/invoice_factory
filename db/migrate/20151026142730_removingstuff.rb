3e4311812b8b6d565ea76e62bec5de92ae749d37
ghrehh <gjburns9@hotmail.com>
2015-10-26 15:15:06 +0000

    sdfsfsdfsdf

--- db/migrate/20151026142730_removingstuff.rb
+++ db/migrate/20151026142730_removingstuff.rb
@@ -1,7 +1,5 @@
 class Removingstuff < ActiveRecord::Migration
   def change
-    remove_column :invs, :address1
-    remove_column :invs, :address2
     remove_column :users, :current_group
     remove_column :users, :address
     remove_column :users, :mobile
