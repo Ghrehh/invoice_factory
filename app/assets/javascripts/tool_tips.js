d576dab247a8862479f801b86806d0ac9d771eb1
ghrehh <gjburns9@hotmail.com>
2015-11-09 05:03:08 +0000

    sdfsdf

---
+++ app/assets/javascripts/tool_tips.js
@@ -0,0 +1,36 @@
+var ready = function() {
+  
+  $("#group-icon").hover(function(){ //tooltip group top
+    $("#group-paragraph-container").animate({ width: '130px' }, 100, function() {
+      $("#group-paragraph").fadeIn( 100 );
+    });
+  }, function(){
+    $("#group-paragraph").fadeOut(100, function( ){
+    $("#group-paragraph-container").animate({ width: '0px' }, 100);
+    });
+  })
+  
+  $("#invoice-icon").hover(function(){ //tooltip group top
+    $("#invoice-paragraph-container").animate({ width: '120px' }, 100, function() {
+      $("#invoice-paragraph").fadeIn(100);
+    });
+  }, function(){
+    $("#invoice-paragraph").fadeOut(100, function(){
+    $("#invoice-paragraph-container").animate({ width: '0px' }, 100);
+    });
+  })
+  
+  $("#edit-icon").hover(function(){ //tooltip group top
+    $("#edit-paragraph-container").animate({ width: '130px' }, 100, function() {
+      $("#edit-paragraph").fadeIn(100);
+    });
+  }, function(){
+    $("#edit-paragraph").fadeOut(100, function(){
+    $("#edit-paragraph-container").animate({ width: '0px' }, 100);
+    });
+  })
+  
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
