7e1b70e2fb1fad0c263120ef71bf2e214a4880f7
ghrehh <gjburns9@hotmail.com>
2015-11-12 20:18:49 +0000

    styled onboarding

--- app/assets/javascripts/tutorial.js
+++ app/assets/javascripts/tutorial.js
@@ -1,15 +1,23 @@
 function tutorial(){
   
   $(".tutorial-button").click(function(){
-    $(this).animate({width: "105px", 
-                    backgroundColor: "#87D687", 
-                    "border-top-color": "#9EF19E", 
-                    "border-left-color": "#9EF19E",
-                    "border-bottom-color": "#78B978",
-                    "border-right-color": "#78B978"
-    }, 200);
-    $(this).val("✔ Continue");
-    button_state = "success"
+    
+    if ($(".tutorial-form").val() == "") {
+      $(".tutorial-error").animate({opacity:"1"});
+    }
+    else {
+      $(this).animate({width: "40px", 
+                      backgroundColor: "#87D687", 
+                      "border-top-color": "#9EF19E", 
+                      "border-left-color": "#9EF19E",
+                      "border-bottom-color": "#78B978",
+                      "border-right-color": "#78B978"
+      }, 200);
+      $(this).val("✔");
+      button_state = "success"
+      $(".tutorial-error").animate({opacity:"0"});
+    }
+    
   });
   
   $(".tutorial-button").hover(function() {
